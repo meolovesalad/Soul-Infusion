fa0c1cefdcb47c2702f15b11988dbb269fd613b2
meolovesalad <loihuy46@gmail.com>
2026-05-20 15:40:23 +0700

    EnemyDead_code

--- Assets/Scripts/Soul/Enemy.cs
+++ Assets/Scripts/Soul/Enemy.cs
@@ -2,5 +2,36 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    [SerializeField] private int _soulDrop = 5;
+    [SerializeField] private GameObject _soulPrefab;
+    private HealthManager _healthManager;
+
+    private void Awake()
+    {
+        _healthManager = GetComponent<HealthManager>();
+    }
+    private void OnEnable()
+    {
+        _healthManager.OnDeath += Die;
+    }
+
+    private void OnDisable()
+    {
+        _healthManager.OnDeath -= Die;
+    }
+
+    private void Die()
+    {
+        int randomSoul = Random.Range(0, _soulDrop);
+
+        for (int i = 0; i < randomSoul; i++) 
+        {
+            Vector3 offset = new Vector3(
+                          Random.Range(-1f, 1f),
+                          Random.Range(-1f, 1f),
+                          0f
+                      );
+            Instantiate(_soulPrefab, transform.position + offset, Quaternion.identity);
+        }
+    }
 }
